1d4f82fc7c9f0e3129d01ffc9f7c3f44fd3f586c
Luiz Felipe Föeger <luizffsan5@gmail.com>
2024-09-10 11:03:58 -0300

    Criação do componente Loading
    Criação do componente Loading

--- src/components/DetalhesFilme/Detalhes.jsx
+++ src/components/DetalhesFilme/Detalhes.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { buscarFilmesId } from '../../API/dadosAPI'
 import { BsGraphUp, BsHourglassSplit, BsWallet2 } from 'react-icons/bs';
 import estilos from './Detalhes.module.css'
+import Loading from '../Loading/Loading';
 // import { MdOutlineAccountBalanceWallet } from 'react-icons/md';
 // import { AiOutlineBarChart } from 'react-icons/ai';
 
@@ -18,36 +19,35 @@ const DetalhesFilme = ({ id }) => {
   };
 
   return (
-    <div>
+    <>
       {movie ? (
-        <>
-          <div className={estilos.detalhesFilme}>
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`Cartaz do filme ${movie.title}`} />
-            <h2 className={estilos.avaliacaoFilme}>{movie.vote_average.toFixed(1)}</h2>
-            <img className={estilos.imgFundo} src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={`Cartaz de fundo do filme ${movie.title}`} />
-            <div>
-              <h1>{movie.title} ({movie.release_date.slice(0, 4)})</h1>
-              <span>"{movie.tagline}"</span>
-              <h3>Data de Lançamento:</h3>
-              <p> {movie.release_date}</p>
+        <div className={estilos.detalhesFilme}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`Cartaz do filme ${movie.title}`} />
+          <h2 className={estilos.avaliacaoFilme}>{movie.vote_average.toFixed(1)}</h2>
+          <img className={estilos.imgFundo} src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={`Cartaz de fundo do filme ${movie.title}`} />
+          <div>
+            <h1>{movie.title} ({movie.release_date.slice(0, 4)})</h1>
+            <span>"{movie.tagline}"</span>
+            <h3>Data de Lançamento:</h3>
+            <p> {movie.release_date}</p>
 
-              <h3>Sinopse:</h3>
-              <p>{movie.overview}</p>
+            <h3>Sinopse:</h3>
+            <p>{movie.overview}</p>
 
-              <h3><BsWallet2 />Orçamento:</h3>
-              <p>{valorEmDolares(movie.budget)}</p>
+            <h3><BsWallet2 />Orçamento:</h3>
+            <p>{valorEmDolares(movie.budget)}</p>
 
-              <h3><BsGraphUp />Faturamento:</h3>
-              <p>{valorEmDolares(movie.revenue)}</p>
+            <h3><BsGraphUp />Faturamento:</h3>
+            <p>{valorEmDolares(movie.revenue)}</p>
 
-              <h3><BsHourglassSplit />Duração:</h3>
-              <p>{movie.runtime} minutos</p>
-            </div></div>
-        </>
+            <h3><BsHourglassSplit />Duração:</h3>
+            <p>{movie.runtime} minutos</p>
+          </div>
+        </div>
       ) : (
-        <h1>Carregando detalhes do filme...</h1>
+        <Loading />
       )}
-    </div>
+    </>
   );
 };
 
